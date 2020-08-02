44bd17f6f4fb9d43bcc6f906ad447426ba07b22d
JosueValladares <jjvd023@gmail.com>
2020-08-02 12:17:45 -0400

    first commit

---
+++ src/helloB20.java
@@ -0,0 +1,7 @@
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
+public class helloB20 {
+    public static void main(String[] args) {
+        System.out.println("hello batch20");
+    }
+}
