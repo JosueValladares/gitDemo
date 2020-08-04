f9277310816d25f714bce82bf0ca47bb5f620791
JosueValladares <jjvd023@gmail.com>
2020-08-04 16:24:40 -0400

    add Happy20.java

---
+++ src/Happy20.java
@@ -0,0 +1,5 @@
+public class Happy20 {
+    public static void main(String[] args) {
+        System.out.println("Hello Batch20");
+    }
+}
--- src/helloB20.java
+++ src/helloB20.java
@@ -2,6 +2,6 @@ import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
 
 public class helloB20 {
     public static void main(String[] args) {
-        System.out.println("hello batch20");
+        System.out.println("hello batch20, this is the first class push to gitHub");
     }
 }
