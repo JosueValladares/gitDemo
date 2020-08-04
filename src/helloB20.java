c1d9892bb0504dccd52154590489ca5ff4a67a8b
JosueValladares <jjvd023@gmail.com>
2020-08-04 16:34:48 -0400

    Update helloB20.java

--- src/helloB20.java
+++ src/helloB20.java
@@ -3,5 +3,6 @@ import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
 public class helloB20 {
     public static void main(String[] args) {
         System.out.println("hello batch20, this is the first class push to gitHub");
+        System.out.println("I love learning new things");
     }
 }
